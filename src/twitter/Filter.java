eaae12835ba9621b3574ca4a668de0c8010f0d4f
xiaozhicong <xiaozhicongZZ@outlook.com>
2023-10-13 23:27:31 +0800

    filter change

--- src/twitter/Filter.java
+++ src/twitter/Filter.java
@@ -3,7 +3,10 @@
  */
 package twitter;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * Filter consists of methods that filter a list of tweets for those matching a
@@ -27,7 +30,14 @@ public class Filter {
      *         in the same order as in the input list.
      */
     public static List<Tweet> writtenBy(List<Tweet> tweets, String username) {
-        throw new RuntimeException("not implemented");
+        username = username.toLowerCase();
+        List<Tweet> tweetsWrittenBy = new ArrayList<>();
+        for (Tweet tweet : tweets) {
+            if (tweet.getAuthor().toLowerCase().equals(username)) {
+                tweetsWrittenBy.add(tweet);
+            }
+        }
+        return tweetsWrittenBy;
     }
 
     /**
@@ -41,7 +51,14 @@ public class Filter {
      *         in the same order as in the input list.
      */
     public static List<Tweet> inTimespan(List<Tweet> tweets, Timespan timespan) {
-        throw new RuntimeException("not implemented");
+        List<Tweet> tweetInSpan = new ArrayList<>();
+        Instant start = timespan.getStart();
+        Instant end = timespan.getEnd();
+        for (Tweet tweet: tweets) {
+            Instant timestamp = tweet.getTimestamp();
+            if ( !start.isBefore(timestamp) && ! end.isAfter(timestamp)) tweetInSpan.add(tweet);
+        }
+        return tweetInSpan;
     }
 
     /**
@@ -60,7 +77,15 @@ public class Filter {
      *         same order as in the input list.
      */
     public static List<Tweet> containing(List<Tweet> tweets, List<String> words) {
-        throw new RuntimeException("not implemented");
+        List<Tweet> matchTweet = new ArrayList<>();
+        for (Tweet tweet: tweets) {
+            String  text = tweet.getText();
+            Pattern pattern = Pattern.compile(text);
+            for (String word: words) {
+                if (pattern.matcher(word).find()) matchTweet.add(tweet);
+            }
+        }
+        return matchTweet;
     }
 
 }
